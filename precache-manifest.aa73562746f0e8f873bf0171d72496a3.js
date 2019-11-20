6410d17edb396fb46f22938979f9a10cd46ac320
RyoyaKaneda-Student <kaneda@ss.cs.osakafu-u.ac.jp>
2019-11-20 19:59:21 +0900

    Updates

---
+++ precache-manifest.aa73562746f0e8f873bf0171d72496a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057249eaf9fc6d9fb19401157a6c93c4",
+    "url": "/RyoyaKaneda-Student/web002.git/index.html"
+  },
+  {
+    "revision": "dd41ff9c3af1e020a7d8",
+    "url": "/RyoyaKaneda-Student/web002.git/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fd68c0d112e6d6a1e356",
+    "url": "/RyoyaKaneda-Student/web002.git/static/js/2.63408274.chunk.js"
+  },
+  {
+    "revision": "dd41ff9c3af1e020a7d8",
+    "url": "/RyoyaKaneda-Student/web002.git/static/js/main.18363580.chunk.js"
+  },
+  {
+    "revision": "dd37f82105d8a2732e1a",
+    "url": "/RyoyaKaneda-Student/web002.git/static/js/runtime-main.46b77397.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/RyoyaKaneda-Student/web002.git/static/media/logo.25bf045c.svg"
+  }
+]);
